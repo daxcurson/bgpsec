122246f45e914e5a67cae9627b55bbfd16bdb365
root <root@192.168.1.6>
2017-12-10 16:14:42 -0500

    Modified configure.ac and Makefile.am to add checks for JAVA_HOME
    Committer: agustin <agustin@flower>

--- bgpsec/src/main/native/routing_configurer.c
+++ bgpsec/src/main/native/routing_configurer.c
@@ -1,17 +1,6 @@
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <time.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <strings.h>
-#include <net/if.h>
 
-#include <libmnl/libmnl.h>
-#include <linux/if_link.h>
-#include <linux/rtnetlink.h>
-
-#include "includes/routing_configurer.h"
+#include "include/routing_configurer.h"
 
 JNIEXPORT void JNICALL Java_MyJNIClass_printf(JNIEnv * env, jobject objc, jstring javaString)
 {
