6ff2b20f10bbe95ff13dca4fe828c356ca84b819
Agustin Villafane <agusvillafane@yahoo.com.ar>
2019-08-18 09:55:09 -0300

    Async message sent!
    Tested a RabbitMQ async message. Seems to work. We'll decide whether to
    use the already existing RabbitMQ infrastructure, or ActiveMQ. RabbitMQ
    has to be installed and run separately, while ActiveMQ is a pure Java
    solution that can be started from within the server. We'll design an
    architecture contemplating both cases.

---
+++ bgpsec/src/test/java/bgpsec/AsyncMessageConsumer.java
@@ -0,0 +1,36 @@
+package bgpsec;
+
+import java.io.IOException;
+
+import org.apache.commons.lang.SerializationUtils;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+import ar.strellis.com.bgp.messages.RouteAdded;
+
+public class AsyncMessageConsumer extends DefaultConsumer
+{
+	private Channel channel;
+	public AsyncMessageConsumer(Channel channel) 
+	{
+		super(channel);
+		this.channel=channel;
+	}
+	public void handleDelivery(String consumerTag,Envelope envelope,AMQP.BasicProperties properties,byte[] body) throws IOException
+	{
+		long deliveryTag = envelope.getDeliveryTag();
+		// (process the message components here ...)
+		
+		String type=properties.getType();
+		if(type.equals("RouteAdded"))
+		{
+			RouteAdded ra=(RouteAdded)SerializationUtils.deserialize(body);
+			System.out.println("Message received asynchronously: "+ra.getMessage());
+		}
+
+		channel.basicAck(deliveryTag, false);
+	}
+}
---
+++ bgpsec/src/test/java/bgpsec/TestAsyncMessageQueue.java
@@ -0,0 +1,48 @@
+package bgpsec;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import org.apache.commons.lang3.SerializationUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.AMQP.BasicProperties;
+
+import ar.strellis.com.bgp.messages.RouteAdded;
+
+public class TestAsyncMessageQueue 
+{
+	private AsyncMessageConsumer consumer;
+	private Channel channel;
+	@Test
+	public void testAsyncMessage()
+	{
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		try (Connection connection = factory.newConnection(); 
+			Channel channel = connection.createChannel()) 
+		{
+			consumer=new AsyncMessageConsumer(channel);
+			this.channel=channel;
+				channel.queueDeclare("AsyncMQ", false, false, false, null);
+				RouteAdded m=new RouteAdded();
+				m.setMessage("HelloAsync");
+				channel.basicConsume("AsyncMQ", false,"ConsumerTag",consumer);
+				channel.basicPublish("", "AsyncMQ", new BasicProperties.Builder().type("RouteAdded").build(), SerializationUtils.serialize(m));
+				System.out.println(" [x] Sent '" + m.getMessage() + "'");
+				Thread.sleep(3L * 1000L);
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (TimeoutException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+}
