aed3274b0959bbacbf4aefa94957427510514009
Agustin <agusvillafane@yahoo.com.ar>
2019-09-21 20:36:34 -0300

    Added another file for the previous change

--- bgpsec/src/main/java/ar/strellis/com/bgpsec/zebra/ZebraSocketMessenger.java
+++ bgpsec/src/main/java/ar/strellis/com/bgpsec/zebra/ZebraSocketMessenger.java
@@ -7,11 +7,14 @@ import java.io.IOException;
 import java.nio.channels.Channels;
 
 import org.apache.commons.lang.SerializationUtils;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
+import com.rabbitmq.client.AMQP.BasicProperties;
 
 import ar.strellis.com.bgp.messages.RouteAdded;
 import ar.strellis.com.bgpsec.messages.AddressAdd;
@@ -25,29 +28,35 @@ import jnr.unixsocket.UnixSocketChannel;
 
 public class ZebraSocketMessenger extends DefaultConsumer implements Runnable
 {
+	private static Logger log=LogManager.getLogger(ZebraSocketMessenger.class); 
+
 	private String zebraSocket;
-	private Channel channel;
+	private Channel channelToZebra;
+	private Channel channelToBgpsec;
 	private DataOutputStream out;
 	private DataInputStream in;
 	private boolean running;
 
-	public ZebraSocketMessenger(Channel channel) 
+	public ZebraSocketMessenger(Channel channelToZebra,Channel channelToBgpsec,String zebraSocket) 
 	{
-		super(channel);
-		this.channel=channel;
+		super(channelToZebra);
+		this.channelToZebra=channelToZebra;
+		this.channelToBgpsec=channelToBgpsec;
+		this.zebraSocket=zebraSocket;
 		running=false;
 	}
-	public ZebraSocketMessenger(Channel channel,String zebraSocket)
+	public ZebraSocketMessenger(Channel channelToZebra,String zebraSocket)
 	{
-		super(channel);
+		super(channelToZebra);
 		this.zebraSocket=zebraSocket;
 	}
 	public void openZebraSocket() throws IOException
 	{
+		log.info("Opening socket to the Zebra daemon located at: "+zebraSocket);
 		File zebraSocketFile=new File(zebraSocket);
 		UnixSocketAddress address = new UnixSocketAddress(zebraSocketFile);
         UnixSocketChannel channel = UnixSocketChannel.open(address);
-        System.out.println("connected to " + channel.getRemoteSocketAddress());
+        log.info("connected to " + channel.getRemoteSocketAddress());
         out=new DataOutputStream(Channels.newOutputStream(channel));
         in=new DataInputStream(Channels.newInputStream(channel));
 	}
@@ -60,9 +69,8 @@ public class ZebraSocketMessenger extends DefaultConsumer implements Runnable
 		if(type.equals("RouteAdded"))
 		{
 			RouteAdded ra=(RouteAdded)SerializationUtils.deserialize(body);
-			System.out.println("Message received asynchronously: "+ra.getMessage());
 		}
-		channel.basicAck(deliveryTag, false);
+		channelToZebra.basicAck(deliveryTag, false);
 		// Now we should inform Zebra that there is a message coming.
 	}
 	@Override
@@ -75,18 +83,31 @@ public class ZebraSocketMessenger extends DefaultConsumer implements Runnable
 		{
 			try {
 				RoutingMessage message=factory.getMessage(r);
+				String messageType="";
 				if(message!=null)
 					System.out.println("The op of this message is "+message.getOp().name());
 				if(message instanceof InterfaceUp)
+				{
 					System.out.println("Interface up");
+					messageType="InterfaceUp";
+				}
 				if(message instanceof InterfaceDown)
+				{
 					System.out.println("Interface down");
+					messageType="InterfaceDown";
+				}
 				if(message instanceof AddressAdd)
+				{
 					System.out.println("Address Add");
+					messageType="AddressAdd";
+				}
 				if(message instanceof AddressDelete)
+				{
 					System.out.println("Address Delete");
-				//int b=r.readUnsignedByte();
-				//System.out.println("Read: "+b);
+					messageType="AddressDelete";
+				}
+				// Here we inform the rest of the server of the kind of message we received from Zebra
+				channelToBgpsec.basicPublish("", "BgpsecQueue", new BasicProperties.Builder().type(messageType).build(), SerializationUtils.serialize(message));
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
